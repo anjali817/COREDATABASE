3e965f0fad36ffc5bdcce4cb3dccf822239aee99
Anjali Rai <99798215+anjali817@users.noreply.github.com>
2022-04-12 11:23:33 +0530

    first commit

---
+++ CoreCRUD/Models/Db_Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCRUD.Models
+{
+    public class Db_Context: DbContext
+    {
+        public Db_Context(DbContextOptions<Db_Context> options) : base(options)
+        {
+        }
+        public DbSet<Employee> employees { get; set; }
+    }
+}
---
+++ CoreCRUD/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
